43880f9ee0bc1c95a12aede2d427304e86004d6f
Cristina <criss.ms7@gmail.com>
2019-05-21 23:06:04 -0500

    adding lsf variables

--- PhysicsTools/NanoAOD/python/jets_cff.py
+++ PhysicsTools/NanoAOD/python/jets_cff.py
@@ -153,12 +153,23 @@ for modifier in run2_miniAOD_80XLegacy, run2_nanoAOD_94X2016:
             looseId = cms.InputTag("looseJetId"),
     )
 
+lepInJetVars = cms.EDProducer("LepInJetProducer",
+     srcPF = cms.InputTag("packedPFCandidates"),
+     src = cms.InputTag("updatedJetsAK8"),
+     srcEle = cms.InputTag("slimmedElectrons"),
+     srcMu = cms.InputTag("slimmedMuons")
+)
+
 updatedJetsAK8WithUserData = cms.EDProducer("PATJetUserDataEmbedder",
      src = cms.InputTag("updatedJetsAK8"),
-     userFloats = cms.PSet(),
+     userFloats = cms.PSet(
+        lsf3 = cms.InputTag("lepInJetVars:lsf3"),
+     ),
      userInts = cms.PSet(
         tightId = cms.InputTag("tightJetIdAK8"),
         tightIdLepVeto = cms.InputTag("tightJetIdLepVetoAK8"),
+        muonIdx3SJ = cms.InputTag("lepInJetVars:muIdx3SJ"),
+        electronIdx3SJ = cms.InputTag("lepInJetVars:eleIdx3SJ"),
      ),
 )
 for modifier in run2_miniAOD_80XLegacy, run2_nanoAOD_94X2016:
@@ -389,7 +400,9 @@ fatJetTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
 		     doc="index of first subjet"),
         subJetIdx2 = Var("?nSubjetCollections()>0 && subjets('SoftDropPuppi').size()>1?subjets('SoftDropPuppi')[1].key():-1", int,
 		     doc="index of second subjet"),
-
+        lsf3 = Var("userFloat('lsf3')",float, doc="LSF (3 subjets)",precision=10),
+        muonIdx3SJ = Var("userInt('muonIdx3SJ')",int, doc="index of muon matched (3 subjets)"),
+        electronIdx3SJ = Var("userInt('electronIdx3SJ')",int,doc="index of electron matched (3 subjets)"),
 #        btagDeepC = Var("bDiscriminator('pfDeepCSVJetTags:probc')",float,doc="CMVA V2 btag discriminator",precision=10),
 #puIdDisc = Var("userFloat('pileupJetId:fullDiscriminant')",float,doc="Pilup ID discriminant",precision=10),
 #        nConstituents = Var("numberOfDaughters()",int,doc="Number of particles in the jet"),
@@ -558,7 +571,8 @@ from RecoJets.JetProducers.QGTagger_cfi import  QGTagger
 qgtagger=QGTagger.clone(srcJets="updatedJets",srcVertexCollection="offlineSlimmedPrimaryVertices")
 
 #before cross linking
-jetSequence = cms.Sequence(jetCorrFactorsNano+updatedJets+tightJetId+tightJetIdLepVeto+bJetVars+jercVars+qgtagger+updatedJetsWithUserData+jetCorrFactorsAK8+updatedJetsAK8+tightJetIdAK8+tightJetIdLepVetoAK8+updatedJetsAK8WithUserData+chsForSATkJets+softActivityJets+softActivityJets2+softActivityJets5+softActivityJets10+finalJets+finalJetsAK8)
+jetSequence = cms.Sequence(jetCorrFactorsNano+updatedJets+tightJetId+tightJetIdLepVeto+bJetVars+jercVars+qgtagger+updatedJetsWithUserData+jetCorrFactorsAK8+updatedJetsAK8+tightJetIdAK8+tightJetIdLepVetoAK8+lepInJetVars+updatedJetsAK8WithUserData+chsForSATkJets+softActivityJets+softActivityJets2+softActivityJets5+softActivityJets10+finalJets+finalJetsAK8)
+
 
 _jetSequence_2016 = jetSequence.copy()
 _jetSequence_2016.insert(_jetSequence_2016.index(tightJetId), looseJetId)
